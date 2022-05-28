d658cb8aebbe827aea36fdb3f14dbab427f0f3ac
NexusMatrix <69479494+wa04sy@users.noreply.github.com>
2022-05-28 16:22:30 +0400

    Delete TwinkLib SFPS GUI Sub-Scripts directory

--- TwinkLib SFPS GUI Sub-Scripts/Auto Collect MiniChests.lua
+++
@@ -1,6 +0,0 @@
-local hrp = game.Players.LocalPlayer.Character.HumanoidRootPart
-for i,v in pairs(game.Workspace.MiniChests:GetChildren()) do
-wait(.5)
-fireclickdetector(v.ClickDetector, 150)
-hrp.CFrame = v.CFrame
-end
